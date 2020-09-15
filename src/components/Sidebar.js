073ad7abff2980c889102c1ffc975a9188b169e6
NJPandey <anjpandey1996@gmail.com>
2020-09-15 13:44:01 +0530

    sidebar is updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Sidebar extends Component {
     render() {
         return (
-            <div>
-                sidebar
+            <div className="sidebar">
+                <div className="user-profile">
+                    <div>
+                        <img alt="txt" />
+                    </div> 
+                    <div>Ronald</div>
+                </div>
+                <div className="user-channel"></div>
             </div>
         );
     }
